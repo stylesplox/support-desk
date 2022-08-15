cbb5b6e2b922245b408c5d9b298d5171d7923465
unknown <you@example.com>
2022-08-15 22:33:34 +0100

    second commit

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const colors = require('colors')
+const dotenv = require('dotenv').config()
+const {errorHandler} = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
+const PORT = process.env.PORT || 5001
+
+//connect to database
+connectDB()
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.get('/', (req, res) =>{
+    res.json({message: 'Hello'})
+})
+
+app.use('/api/users', require('./routes/userRoutes'))
+app.use('/api/tickets', require('./routes/ticketRoutes'))
+
+app.use(errorHandler)
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
